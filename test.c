6b6c0482de3d53adf9a2e8c490056c0bdf18a944
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-12-04 14:25:29 +0000

    Add medium type

--- test.c
+++ test.c
@@ -5,12 +5,14 @@
 int main(int argc, char ** argv) {
 
   small_t s = {0};
+  medium_t m = {0};
 
   assert(function1(5) == 2);
   assert(function2()  == 2);
   assert(function1(20) == 21);
 
   function3(s);
+  function4(m);
 
   return 0;
 }
